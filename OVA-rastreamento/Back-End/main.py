4cf52e560cf4defb4ba1a7aadcca0bffdd247d9d
duducaa <duduca1881@gmail.com>
2024-03-25 17:36:39 -0300

    fixed the bug of logging

--- OVA-rastreamento/Back-End/main.py
+++ OVA-rastreamento/Back-End/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import logging
+import json
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -11,9 +12,13 @@ logging.basicConfig(level=logging.INFO, filename="../ova.log")
 def log():
     if request.method == 'POST':
         # request.get_json() parse a JSON data, but for the future we need use request.form() to get key/value pairs from the body of HTML forms - For more information: https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
-        log_request = request.get_json()
-        log_data = log_request[0]
-        logging.info(log_data)
+        try:
+            log_request = request.get_json()
+            log_data = log_request[0]
+            logging.info(log_data)
+        except Exception:
+            return json.dumps({"message", "erro"})
+        return json.dumps(log_data)
     else:
         return "Wrong Request Methods. Just POST Allowed", 405
 
