3d72d75bec44bdcdc6a6c8d3381db7c17f7e2ea5
duducaa <duduca1881@gmail.com>
2024-03-27 21:26:54 -0300

    excluded column_name

--- OVA-rastreamento/Back-End/data/models/course.py
+++ OVA-rastreamento/Back-End/data/models/course.py
@@ -2,8 +2,8 @@ from base import BaseModel
 from peewee import *
 
 class Course(BaseModel):
-    course_id = IntegerField(primary_key=True, column_name="COURSE_ID")
-    course_name = TextField(column_name="COURSE_NAME")
+    course_id = IntegerField(primary_key=True)
+    course_name = TextField()
     
     class Meta:
         table_name = 'COURSE'
--- OVA-rastreamento/Back-End/data/models/student.py
+++ OVA-rastreamento/Back-End/data/models/student.py
@@ -3,10 +3,10 @@ from course import Course
 from peewee import *
 
 class Student(BaseModel):
-    ra = TextField(primary_key=True, column_name="RA")
-    student_password = TextField(column_name="STUDENT_PASSWORD")
-    student_name = TextField(column_name="STUDENT_NAME")
-    course_id = ForeignKeyField(Course, backref="students", on_delete="CASCADE", on_update="CASCADE", column_name="COURSE_ID")
+    ra = TextField(primary_key=True)
+    student_password = TextField()
+    student_name = TextField()
+    course_id = ForeignKeyField(Course, backref="students", on_delete="CASCADE", on_update="CASCADE")
     
     class Meta:
         table_name = 'STUDENT'
