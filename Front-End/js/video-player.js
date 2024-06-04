7a106a4021cad4ef0b3eef50044705f4e2468250
duducaa <duduca1881@gmail.com>
2024-06-04 14:34:55 -0300

    finished the comments

--- Front-End/js/video-player.js
+++ Front-End/js/video-player.js
@@ -1,13 +1,16 @@
+// basic configurations for the url
 const PORT = 8000;
 const HOST = "localhost";
 const BASE_URL = `http://${HOST}:${PORT}`
 
+// the lines above add dinamically the script for 
+// the youtube embed api in the ova html
 var tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+// generate the checkpoints for the video watching progress
 function generatePoints(x, video_id) {
     const perc_done = localStorage.getItem(`${video_id}_viewed`);
     const points = {};
@@ -21,9 +24,12 @@ function generatePoints(x, video_id) {
 
 let players = [];
 
+// the function is called when the API is ready
 function onYouTubeIframeAPIReady() {
     const videoIframes = document.querySelectorAll(".ova_video");
     videoIframes.forEach(iframe => {
+        // for each video in the OVA, a player is created with an id
+        // an API object and the checkpoints
         const videoId = iframe.dataset.videoId
         if (localStorage.getItem(`${videoId}_viewed`) == null) {
             localStorage.setItem(`${videoId}_viewed`, 0);
@@ -44,12 +50,16 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
+// this function is called when each player is ready
 function onPlayerReady(event) {
+    // get the player data
     const player = event.target;
     const iframe = player.getIframe();
     const iframeId = iframe.dataset.playerlistPos;
     const playerData = players[iframeId];
     player.stopVideo();
+    // for each second, the script will verify if the user reached
+    // a new checkpoint
     setInterval(function() {
         const ct = player.getCurrentTime();
         const d = player.getDuration();
@@ -59,10 +69,14 @@ function onPlayerReady(event) {
                 if (perc >= point & !playerData.points[point]) {
                     playerData.points[point] = true;
                     localStorage.setItem(`${playerData.id}_viewed`, ct);
+                    // the interaction registration is made, 
+                    // identifying the percentage and the video
                     videoVisualization(`Watched ${point}% of the ${iframe.title} video`)
                     .then(response => console.log("success"))
                     .catch(error => console.log(error));
                 }
+                // this is especially for 100%, once the
+                // video has ended, but its equal to the registration above
                 if (player.getPlayerState() == 0 & !playerData.points[100]) {
                     playerData.points[100] = true;
                     localStorage.setItem(`${playerData.id}_viewed`, ct);
@@ -75,11 +89,13 @@ function onPlayerReady(event) {
     }, 1000);
 }
 
+// this function is called when the video is paused, played, etc
 function onPlayerStateChange(event) {
     const player = event.target;
     const iframe = player.getIframe();
     const iframeId = iframe.dataset.playerlistPos - 1;
     const playerData = players[iframeId];
+    // this ensure that the video will start in 0
     if (event.data == YT.PlayerState.PLAYING && !playerData.initial) {
         player.mute();
         player.seekTo(0);
@@ -88,7 +104,10 @@ function onPlayerStateChange(event) {
     }
 }
 
+// the function to make the registration of the interaction
 function videoVisualization(perc_watched, type="POST") {
+    // contains the id of the student that is watching, the id of the ova
+    // and the percentede watched
     const data = {
         student_id: localStorage.getItem("student_id"),
         ova_id: localStorage.getItem("ova_id"),
