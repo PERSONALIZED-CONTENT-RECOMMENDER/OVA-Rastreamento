e83df71e9f210f0cda576cdc43b2af4899dc8386
duducaa <duduca1881@gmail.com>
2024-06-11 10:11:23 -0300

    can connect to aws

--- Back-End/data/models/base.py
+++ Back-End/data/models/base.py
@@ -1,7 +1,13 @@
 from peewee import * # ORM
 
 # estabilishment of the database connection
-db = MySQLDatabase("ova_db", user="root", host="localhost", password="Password-123", port=3306)
+db = MySQLDatabase(
+    "ova_db",
+    user="remote",
+    password="Password-123",
+    host="ec2-3-139-95-211.us-east-2.compute.amazonaws.com",
+    port=3306
+)
 
 class BaseModel(Model):
     class Meta:
--- test/aws/main.py
+++ test/aws/main.py
@@ -1,3 +1,11 @@
-import paramiko
+from peewee import MySQLDatabase
 
-host_ip=""
+db = MySQLDatabase(
+    "ova_db",
+    user="remote",
+    password="Password-123",
+    host="ec2-3-139-95-211.us-east-2.compute.amazonaws.com",
+    port=3306
+)
+
+db.connect()
