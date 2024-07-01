74a2001b06fe5097498cf7c54b0026059f44f7bf
Eduardo Fiscina Menezes Moraes <duduca1881@gmail.com>
2024-07-01 18:57:27 -0300

    changed ec2 instance

--- Back-End/data/models/base.py
+++ Back-End/data/models/base.py
@@ -5,7 +5,7 @@ db = MySQLDatabase(
     "ova_db",
     user="remote",
     password="Password-123",
-    host="ec2-3-139-95-211.us-east-2.compute.amazonaws.com",
+    host="ec2-54-162-164-125.compute-1.amazonaws.com",
     port=3306
 )
 
