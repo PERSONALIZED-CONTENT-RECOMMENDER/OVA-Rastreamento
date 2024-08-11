ca4cbd4cea9b97426cf478a666720a92d49ba118
duducaa <duduca1881@gmail.com>
2024-08-11 03:24:15 -0300

    deleted the section tabs from the ovas

--- Back-End/data/models/base.py
+++ Back-End/data/models/base.py
@@ -1,22 +1,21 @@
 from peewee import * # ORM
 
 # estabilishment of the database connection
-db = MySQLDatabase(
-    "ova_db",
-    user="remote",
-    password="OvaIa2024cimatec-mysql",
-    host="ec2-54-236-209-79.compute-1.amazonaws.com",
-    port=3306
-)
-
-# option to local development
 # db = MySQLDatabase(
 #     "ova_db",
-#     user="root",
-#     password="Password-123",
-#     host="localhost",
+#     user="remote",
+#     password="OvaIa2024cimatec-mysql",
+#     host="ec2-54-236-209-79.compute-1.amazonaws.com",
 #     port=3306
 # )
+
+# option to local development
+db = MySQLDatabase(
+    "ova_db",
+    user="root",
+    host="localhost",
+    port=3306
+)
 
 # base class for the others classes
 class BaseModel(Model):
