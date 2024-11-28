ad17c2c69e2f7e87d3cbec99a586dcbe66b49ba7
duducaa <duduca1881@gmail.com>
2024-11-28 02:20:01 -0300

    only html, css and js files

--- Back-End/data/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from students import Students
-from ovas import OVAs
-from interactions import Interactions
-from courses import Courses
-from offerings import Offerings
-from subjects import Subjects
-from competencies import Competencies
